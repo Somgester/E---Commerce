4206b1c6e89b6873f679c3e297145070d3f491d1
Somgester <dhruvratmele13@gmail.com>
2025-01-15 16:54:26 +0530

    navbar final

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
             src={assets.profile_icon}
             alt="Cart Icon"
           />
-          <div className="group hover:block hiden absolute dropdown-menu right-0 pt-4">
+          <div className="hidden group-hover:block absolute dropdown-menu right-0 pt-4">
             <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
               <p className="cursor-pointer hover:text-black">My Profile</p>
               <p className="cursor-pointer hover:text-black">Orders</p>
@@ -64,13 +64,16 @@ const Navbar = () => {
           visible ? "w-full" : "w-0"
         }`}
       >
-        <img
-          onClick={() => setVisible(false)}
-          src={assets.cross_icon}
-          className="absolute top-4 right-4 w-5 cursor-pointer"
-          alt="Close Icon"
-        />
-        <p>hello</p>
+        <div className='flex flex-col text-gray-600'>
+          <div onClick={()=>setVisible(false)} className='flex items-center gap-4 p-3'>
+            <img className="h-4 rotate-180" src= {assets.dropdown_icon} alt="Dropdown Icon" />
+            <p>Back</p>
+          </div>
+          <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to="/">HOME</NavLink>
+          <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to="/collection">COLLECTION</NavLink>
+          <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to="/about">ABOUT</NavLink>
+          <NavLink onClick={()=>setVisible(false)} className='py-2 pl-6 border' to="/contact">CONTACT</NavLink>
+        </div>
       </div>
     </div>
   );
